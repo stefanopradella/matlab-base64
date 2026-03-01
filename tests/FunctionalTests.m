e705c8ebfaad5c6c792e8678ef3d5d20c60cbef2
stefanopradella <stefano.pradella.97@gmail.com>
2026-03-01 22:23:20 +0100

    Added tests for Base64URL

--- tests/FunctionalTests.m
+++ tests/FunctionalTests.m
@@ -31,7 +31,37 @@ classdef FunctionalTests < matlab.unittest.TestCase
             end
         end
         
-        
+        function randomisedEncodingURLMode(testCase)
+
+            for iIteration = 1:testCase.nRandomisedTestIterations
+                inputData = uint8(randi([0 2^8-1], 1, randi([1 testCase.maxBytesRandomisedTest])));
+                result = base64Encode(inputData, true);
+                expectedValue = matlab.net.base64encode(inputData);
+                expectedValue = strrep(expectedValue, '+', '-');
+                expectedValue = strrep(expectedValue, '/', '_');
+                expectedValue = strrep(expectedValue, '=', '');
+
+                testCase.verifyEqual(result, expectedValue);
+            end
+        end
+
+       function randomisedDecodingURLMode(testCase)
+            
+            for iIteration = 1:testCase.nRandomisedTestIterations
+                inputData = uint8(randi([0 2^8-1], 1, randi([1 testCase.maxBytesRandomisedTest])));
+                encodedData = matlab.net.base64encode(inputData);
+
+                encodedDataURLMode = encodedData;
+                encodedDataURLMode = strrep(encodedDataURLMode, '+', '-');
+                encodedDataURLMode = strrep(encodedDataURLMode, '/', '_');
+                encodedDataURLMode = strrep(encodedDataURLMode, '=', '');
+
+                result = base64Decode(encodedDataURLMode, true);
+                expectedValue = matlab.net.base64decode(encodedData);
+
+                testCase.verifyEqual(result, expectedValue);
+            end
+        end
     end
 
 end
