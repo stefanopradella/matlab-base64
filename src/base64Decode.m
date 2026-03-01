c058b8f169536b1bdc70924c935a0a49064db31a
stefanopradella <stefano.pradella.97@gmail.com>
2026-03-01 22:23:01 +0100

    Add support for decoding of Base64URL mode

--- src/base64Decode.m
+++ src/base64Decode.m
@@ -7,33 +7,35 @@ function decodedData = base64Decode(inputData, urlmode)
     
     coder.varsize("decodedData", [1, Inf], true);
 
-    base64DecodeTable      =   coder.const(uint8([zeros(1, 42), 63, zeros(1, 3), 64, 53:62, zeros(1, 7), 1:26, zeros(1, 6), 27:52]) - 1);
-    
-    
-    if urlmode
+    base64DecodeTable       =   coder.const(uint8([zeros(1, 42), 63, zeros(1, 3), 64, 53:62, zeros(1, 7), 1:26, zeros(1, 6), 27:52]) - 1);
+    base64URLDecodeTable    =   coder.const(uint8([zeros(1, 44), 63, zeros(1, 2), 53:62, zeros(1, 7), 1:26, zeros(1, 4), 64, 0, 27:52]) - 1);
 
+    if urlmode
+        inputData = [inputData, repelem('=', 1, (ceil(size(inputData, 2)/4)*4) - size(inputData, 2))];
+        inputSize = size(inputData, 2);
+        decodedBytes = base64URLDecodeTable(inputData);
+        decodedData = uint8(zeros(1, ceil(inputSize*3/4)));
     else
         inputSize = size(inputData, 2);
-
-        decodedData = uint8(zeros(1, inputSize*3/4));
-
         decodedBytes = base64DecodeTable(inputData);
+        decodedData = uint8(zeros(1, inputSize*3/4));
+    end
 
-        for iBlock = 0:(inputSize/4)-1
-            startIdx_dec = iBlock*3;
-            startIdx_enc = iBlock*4;
-            decodedData(startIdx_dec + 1) = bitor(bitshift(decodedBytes(startIdx_enc + 1), 2), bitshift(decodedBytes(startIdx_enc + 2), -4));
-            decodedData(startIdx_dec + 2) = bitor(bitshift(decodedBytes(startIdx_enc + 2), 4), bitshift(decodedBytes(startIdx_enc + 3), -2));
-            decodedData(startIdx_dec + 3) = bitor(bitshift(decodedBytes(startIdx_enc + 3), 6), decodedBytes(startIdx_enc + 4));
-        end
+    
+    for iBlock = 0:(inputSize/4)-1
+        startIdx_dec = iBlock*3;
+        startIdx_enc = iBlock*4;
+        decodedData(startIdx_dec + 1) = bitor(bitshift(decodedBytes(startIdx_enc + 1), 2), bitshift(decodedBytes(startIdx_enc + 2), -4));
+        decodedData(startIdx_dec + 2) = bitor(bitshift(decodedBytes(startIdx_enc + 2), 4), bitshift(decodedBytes(startIdx_enc + 3), -2));
+        decodedData(startIdx_dec + 3) = bitor(bitshift(decodedBytes(startIdx_enc + 3), 6), decodedBytes(startIdx_enc + 4));
+    end
 
-        % Remove padding
-        if inputData(end) == '='
-            if inputData(end-1) == '='
-                decodedData = decodedData(1:end-2);
-            else
-                decodedData = decodedData(1:end-1);
-            end
+    % Remove padding
+    if inputData(end) == '='
+        if inputData(end-1) == '='
+            decodedData = decodedData(1:end-2);
+        else
+            decodedData = decodedData(1:end-1);
         end
     end
 end
