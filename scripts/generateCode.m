f21887ce13bd94024c25175ec889865eb8c81669
stefanopradella <stefano.pradella.97@gmail.com>
2026-04-06 19:26:50 +0200

    Update generateCode to unify common code

--- scripts/generateCode.m
+++ scripts/generateCode.m
@@ -2,7 +2,4 @@ inputTypes = cell(1, 1);
 inputTypes{1} = coder.newtype("uint8", [Inf 1]);
 inputTypes{2} = coder.newtype("logical", [1 1]);
 
-
-codegen -config getCoderConfig() -report base64Encode.m -args inputTypes
-
-codegen -config getCoderConfig() -report base64Decode.m -args inputTypes
+codegen -config getCoderConfig() -report -o matlab_base64 base64Encode.m -args inputTypes base64Decode.m -args inputTypes
